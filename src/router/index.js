afb4dc02d142e59024cedff88ddd436439109152
linqiu <1131868300@qq.com>
2022-03-02 17:55:36 +0800

    router bug

--- src/router/index.js
+++ src/router/index.js
@@ -11,17 +11,11 @@ const routes = [{
     name: 'Home',
     component: () => import('../views/Home'),
   },
-  // {
-  //   path: '/about',
-  //   name: 'About',
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  // },
   {
     path: '/home',
-    redirect: '/'
+    redirect: '/',
+    component: () => import('../views/Home'),
+
   },
   {
     path: '/bridge',
