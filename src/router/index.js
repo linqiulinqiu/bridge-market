4fc4d2e585155d5dc19c989af8835e7d47afc6b0
linqiu <1131868300@qq.com>
2022-03-02 17:40:05 +0800

    revise router mistake

--- src/router/index.js
+++ src/router/index.js
@@ -20,8 +20,8 @@ const routes = [{
   //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   // },
   {
-    path: '/',
-    redirect: '/Home'
+    path: '/home',
+    redirect: '/home'
   },
   {
     path: '/bridge',
