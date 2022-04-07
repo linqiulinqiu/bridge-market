2be2652644acb5f0e430785b550cc8a659609c82
linqiu <1131868300@qq.com>
2022-04-07 16:04:58 +0800

    test

--- src/swap.js
+++ src/swap.js
@@ -5,60 +5,60 @@ const pbw = require('pbwallet')
 //  WCoin: all pair with PBP
 //  BNB: pair with PBP
 //  Others: pair with WBNB
-async function path_for_pair(bsc, from, to){
+async function path_for_pair(bsc, from, to) {
     const paths = []
     from = ethers.utils.getAddress(from)
     to = ethers.utils.getAddress(to)
-    if(from==to) return false
-    if(from==ethers.constants.AddressZero){ // BNB
+    if (from == to) return false
+    if (from == ethers.constants.AddressZero) { // BNB
         paths.push(bsc.ctrs.wbnb.address)
-    }else{
+    } else {
         paths.push(from)
-        if(pbw.wcoin_info(from,'address')){ // from is WCoin, need PBP
+        if (pbw.wcoin_info(from, 'address')) { // from is WCoin, need PBP
             paths.push(bsc.ctrs.pbp.address)
-        }else if(from!=bsc.ctrs.pbp.address){   // all other from should use WBNB
+        } else if (from != bsc.ctrs.pbp.address) { // all other from should use WBNB
             paths.push(bsc.ctrs.wbnb.address)
         }
     }
-    if(to==ethers.constants.AddressZero){
-        if(paths[paths.length-1]!=bsc.ctrs.wbnb.address){
+    if (to == ethers.constants.AddressZero) {
+        if (paths[paths.length - 1] != bsc.ctrs.wbnb.address) {
             paths.push(bsc.ctrs.wbnb.address)
         }
-    }else{
-        if(pbw.wcoin_info(to,'address')){
-            if(paths[paths.length-1]!=bsc.ctrs.pbp.address){  // to is WCoin, and no PBP at tail of path
+    } else {
+        if (pbw.wcoin_info(to, 'address')) {
+            if (paths[paths.length - 1] != bsc.ctrs.pbp.address) { // to is WCoin, and no PBP at tail of path
                 paths.push(bsc.ctrs.pbp.address)
             }
-        }else if(to!=bsc.ctrs.pbp.address&&paths[paths.length-1]!=bsc.ctrs.wbnb.address){   // all other from should use WBNB
+        } else if (to != bsc.ctrs.pbp.address && paths[paths.length - 1] != bsc.ctrs.wbnb.address) { // all other from should use WBNB
             paths.push(bsc.ctrs.wbnb.address)
         }
-        if(paths[paths.length-1]!=to){
+        if (paths[paths.length - 1] != to) {
             paths.push(to)
         }
     }
     return paths
 }
 
-async function estimate(bsc,from, to, amount_in){
+async function estimate(bsc, from, to, amount_in) {
     const paths = await path_for_pair(bsc, from, to)
     const aouts = await bsc.ctrs.router.getAmountsOut(amount_in, paths)
-    return aouts[aouts.length-1]
+    return aouts[aouts.length - 1]
 }
 
-async function swap(bsc, from, to, amount_in, min_out, lasting){
-    const deadline = parseInt((new Date()).getTime()/1000) + lasting
-    const paths = await path_for_pair(bsc,from, to)
+async function swap(bsc, from, to, amount_in, min_out, lasting) {
+    const deadline = parseInt((new Date()).getTime() / 1000) + lasting
+    const paths = await path_for_pair(bsc, from, to)
     let receipt = false
-    if(paths[0]==bsc.ctrs.wbnb.address){
+    if (paths[0] == bsc.ctrs.wbnb.address) {
         receipt = await bsc.ctrs.router.swapExactETHForTokens(min_out, paths, bsc.addr, deadline, {
             value: amount_in
         })
-    }else if(paths[paths.length-1]==bsc.ctrs.wbnb.address){
+    } else if (paths[paths.length - 1] == bsc.ctrs.wbnb.address) {
         receipt = await bsc.ctrs.router.swapExactTokensForETH(amount_in, min_out, paths, bsc.addr, deadline)
-    }else{
+    } else {
         receipt = await bsc.ctrs.router.swapExactTokensForTokens(amount_in, min_out, paths, bsc.addr, deadline)
     }
-    return receipt.hash
+    return receipt
 }
 exports.estimate = estimate
 exports.swap = swap
