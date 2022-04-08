49e3ac2e1b235da9ded1a73790c271d2689dd19d
hutao9323 <1026578397@qq.com>
2022-04-08 15:08:38 +0800

    update doc

--- src/components/document/Contact.vue
+++ src/components/document/Contact.vue
@@ -2,12 +2,9 @@
   <el-col>
     <h1>Contact us</h1>
     <el-col>
-      <h2>联系我们:</h2>
-      <el-col>
-        <p>- Discord: <a href="https://discord.com/invite/xHC9fBfeVW" target="_blank">https://discord.com/invite/xHC9fBfeVW</a></p>
-        <p>- 电报: <a href="https://t.me/PlotBridge" target="_blank">https://t.me/PlotBridge</a></p>
-        <p>- 推特: <a href="https://twitter.com/plot_bridge" target="_blank">https://twitter.com/plot_bridge</a></p>
-      </el-col>
+        <p>- Discord: <a href="https://discord.com/invite/xHC9fBfeVW" target="_blank">https://discord.com/invite/xHC9fBfeVW</a> <br>
+        - 电报: <a href="https://t.me/PlotBridge" target="_blank">https://t.me/PlotBridge</a><br>
+        - 推特: <a href="https://twitter.com/plot_bridge" target="_blank">https://twitter.com/plot_bridge</a></p>
      </el-col>
   </el-col>
 </template>
