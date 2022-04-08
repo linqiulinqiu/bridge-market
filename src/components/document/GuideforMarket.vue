4a5781a6ec66a419d9a24382369fead268bdf0b3
hutao9323 <1026578397@qq.com>
2022-04-08 18:18:07 +0800

    en doc

--- src/components/document/GuideforMarket.vue
+++ src/components/document/GuideforMarket.vue
@@ -1,10 +1,8 @@
 <template> 
   <el-col>
-    <h1>Guide For Market</h1>
+    <h1>{{$t('guide-market')}}</h1>
     <el-col>
-      <p>
-        在市场中，您可以根据您的喜好购买NFT，也可以上架自己的NFT进行出售，目前支持用BNB和BUSD来出售或者购买NFT。
-      </p>
+      <p>{{$t('guide-market1')}}</p>
     </el-col>
     
   </el-col>
