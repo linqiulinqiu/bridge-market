0b8fb0ff12f660d2a9ec955cd175aff55601adb0
dxl <dxl@plotbridge.com>
2022-04-14 16:43:14 +0800

    testing multiple stake

--- src/views/Stake.vue
+++ src/views/Stake.vue
@@ -1,119 +1,38 @@
 <template>
-  <el-col id="stake" v-if="bsc.addr">
-    <el-container v-if="'addr' in bsc">
-      <el-main>
-        <el-col
-          class="stake-main"
-          :lg="{ span: 10, offset: 7 }"
-          :md="{ span: 12, offset: 6 }"
-          :sm="{ span: 16, offset: 4 }"
-          :xs="{ span: 22, offset: 1 }"
-        >
-         <el-col id="staketitle">
-           <h2>Earn by Staking</h2>
-           <el-button @click="refresh">refresh</el-button>
-         </el-col>
-         <el-col id="stakeinput">
-           <p>WXCC质押：{{farm_amount }}WXCC</p> <!-- 显示已质押金额 -->
-           <span>已赚取：{{ earned_amount }}PBP</span> <!-- 显示目前的收益 -->
-           <el-button @click="claim">claim</el-button>
-         </el-col>
-         <el-col id="stakeapprove"> 
-           <el-button @click="dia_set_amount = true">stake</el-button>
-           <el-button @click="dia_withdraw= true">withdraw</el-button>
-         </el-col>
-        </el-col>
-      </el-main>
+  <el-col id="stake">
+    <el-container v-if="stakeTokens.length">
+      <stake-item v-for="item in stakeTokens" :stakeAddr="item.stakeAddr" :pid="item.pid" :key="item.pid">
+      </stake-item>
     </el-container>
-    <el-col class="info" v-else>
-      <h2>{{ $t("look-info") }}</h2>
-    </el-col>
-    <el-dialog :visible.sync="dia_set_amount">
-      <el-card>
-        <h2>设置质押数量</h2>
-        <p><span>balance：{{ stk_balance }}WXCC</span><el-button @click="stake_amount=stk_balance">all</el-button></p><!-- 显示钱包中WXCC余额 -->
-        <el-input  
-        v-model="stake_amount"
-        clearable></el-input>
-        <ApproveButton
-          v-if="stk_balance"
-          :bsc="bsc"
-          :token="bsc.ctrs.wxcc.address"
-          :spender="bsc.ctrs.staking.address"
-          :min-req="stk_balance_bn"
-        >
-            <el-button @click="stake_token">deposit</el-button>
-        </ApproveButton>
-      </el-card>
-    </el-dialog>
-    <el-dialog :visible.sync="dia_withdraw">
-      <el-card>
-        <h2>Withdraw</h2>
-        <el-input  
-        v-model="withdraw_amount"
-        clearable></el-input>
-        <el-button @click="withdraw">withdraw</el-button>
-      </el-card>
-    </el-dialog>
-
+    <el-button @click="refresh" v-else>Reload All</el-button>
   </el-col>
 </template>
 <script>
-import tokens from '../tokens';
-import ApproveButton from "../components/lib/ApproveButton.vue";
+import StakeItem from "../components/StakeItem.vue";
 import { mapState } from "vuex";
 export default {
   name: "Stake",
   components: {
-    ApproveButton,
+    StakeItem,
   },
   computed: mapState({
     bsc: "bsc"
   }),
   data(){
     return{
-      farm_amount:'',
-      earned_amount:'',
-      stk_balance:'',
-      stk_balance_bn: 0,
-      stake_amount:0,
-      withdraw_amount:0,
-      dia_set_amount: false,
-      dia_withdraw: false
+      stakeTokens:[]
     }
   },
   methods: {
     refresh: async function(){
-        const stk_addr = this.bsc.ctrs.wxcc.address
-        this.stk_balance_bn = await tokens.balance(stk_addr)
-        this.stk_balance = await tokens.format(stk_addr, this.stk_balance_bn)
-        const earnval = await this.bsc.ctrs.staking.earned(this.bsc.addr)
-        this.earned_amount = await tokens.format(this.bsc.ctrs.pbp.address, earnval)
-        console.log('earned val', earnval)
-    },
-    withdraw: async function(){
-        const stk_addr = this.bsc.ctrs.wxcc.address
-        const amount = await tokens.parse(stk_addr, this.withdraw_amount)
-        if(amount.gt(0)){
-            const receipt = await this.bsc.ctrs.staking.withdraw(amount)
-            console.log('withdraw receipt',receipt)
+        const tokens = await this.bsc.ctrs.staking.pools()
+        const stk = []
+        for(let i in tokens){
+            stk.push({stakeAddr: tokens[i], pid: i})
         }
-    },
-    claim: async function(){
-        const receipt = await this.bsc.ctrs.staking.claimReward()
-        console.log('claim receipt', receipt)
-        console.log('领取收益');
-    },
-    stake_token: async function(){
-        const stk_addr = this.bsc.ctrs.wxcc.address
-        const amount = await tokens.parse(stk_addr, this.stake_amount)
-        if(amount.gt(0)&&amount.lte(this.stk_balance_bn)){
-            const receipt = await this.bsc.ctrs.staking.stake(amount)
-            console.log('stake receipt', receipt)
-        }else{
-            console.log('Invalid amount', amount);
-        }
-    },
+        this.stakeTokens = stk
+        console.log('stake tokens', this.stakeTokens)
+    }
   }
 };
 </script>
