22d63b89b34af231d2ba5410d3df75dca56b806e
linqiu <1131868300@qq.com>
2022-04-19 17:37:33 +0800

    add style

--- src/components/PresellMain.vue
+++ src/components/PresellMain.vue
@@ -4,9 +4,16 @@
     <p>价格：--</p>
     <p>预计价格：--</p>
     <p>余额：</p>
-    <el-input></el-input>
+    <el-input class="preinput"></el-input>
+    <el-button>最大购买量</el-button>
     <el-button>购买</el-button>
   </el-col>
 </template>
 <script>
 </script>
+<style>
+.preinput {
+  width: 200px;
+  margin-right: 10px;
+}
+</style>
