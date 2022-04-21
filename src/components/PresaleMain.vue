47fc04c58a66ded2dae2f72a7fb165da012c9dcb
dxl <dxl@plotbridge.com>
2022-04-21 10:26:19 +0800

    presale hide when end

--- src/components/PresaleMain.vue
+++ src/components/PresaleMain.vue
@@ -1,35 +1,43 @@
 <template>
   <el-col id="presaleMain">
-    <p>{{ time_msg }}</p>
-    <p>{{ $t("stage") }}：{{ stage }}</p>
-    <p>
-      {{ $t("remainder") }}：<span class="font">{{ remain }}</span>
-      {{ coinName }}
-    </p>
-    <p>
-      {{ $t("purchase") }}：<span class="font">{{ buyable }}</span>
-      {{ coinName }}
-    </p>
-    <p>
-      {{ $t("price") }}：<span class="font">{{ price_str }}</span> BNB
-    </p>
-    <p>
-      {{ $t("prepay") }}：<span class="font">{{ payment }} </span>BNB
-    </p>
-    <p>
-      {{ $t("balance") }}：
-      <span class="font"> {{ balance }} {{ coinName }} </span>
-    </p>
-    <el-input
-      v-model="amount"
-      class="preinput"
-      clearable
-      suffix-icon="el-icon-edit"
-    ></el-input>
-    <el-button @click="max_amount" type="primary">{{ $t("max") }}</el-button>
-    <el-button @click="buy" :loading="buy_loading" type="primary">
-      {{ $t("buy") }}
-    </el-button>
+    <el-col v-if="pstat=='s'">
+        <p>{{ time_msg }}</p>
+        <p>{{ $t("stage") }}：{{ stage }}</p>
+        <p>
+          {{ $t("remainder") }}：<span class="font">{{ remain }}</span>
+          {{ coinName }}
+        </p>
+        <p>
+          {{ $t("purchase") }}：<span class="font">{{ buyable }}</span>
+          {{ coinName }}
+        </p>
+        <p>
+          {{ $t("price") }}：<span class="font">{{ price_str }}</span> BNB
+        </p>
+        <p>
+          {{ $t("prepay") }}：<span class="font">{{ payment }} </span>BNB
+        </p>
+        <p>
+          {{ $t("balance") }}：
+          <span class="font"> {{ balance }} {{ coinName }} </span>
+        </p>
+        <el-input
+          v-model="amount"
+          class="preinput"
+          clearable
+          suffix-icon="el-icon-edit"
+        ></el-input>
+        <el-button @click="max_amount" type="primary">{{ $t("max") }}</el-button>
+        <el-button @click="buy" :loading="buy_loading" type="primary">
+          {{ $t("buy") }}
+        </el-button>
+    </el-col>
+    <el-col v-else-if="pstat=='e'">
+        <p>Presale ended</p>
+    </el-col>
+    <el-col v-else>
+        <p>Loading</p>
+    </el-col>
   </el-col>
 </template>
 <script>
@@ -47,6 +55,7 @@ export default {
   },
   data() {
     return {
+      pstat: 'l',
       coinName: "PBP",
       stakeTokens: [],
       time_msg: "",
@@ -77,7 +86,12 @@ export default {
     refresh: async function () {
       const token = this.bsc.ctrs.pbp.address;
       const toend = (await this.bsc.ctrs.presale.timeRemains()).toNumber();
-      this.time_msg = times.formatRelTS(Math.floor(Date.now()/1000)+toend)
+      if(toend==0){
+          this.pstat = 'e';
+      }else{
+          this.time_msg = times.formatRelTS(Math.floor(Date.now()/1000)+toend)
+          this.pstat = 's';
+      }
       const pkgs = await this.bsc.ctrs.presale.pkgs();
       for (let i in pkgs[0]) {
         const remain = pkgs[0][i].sub(pkgs[1][i]);
